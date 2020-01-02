330bcfca83967a4378b615eefdc6ee362946592a
Jędrzej Krawczyk <siewierjedr@gmail.com>
2020-01-02 13:29:24 +0100

    Fix fetch problem for old version of browsers // Add user input validation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import './assets/scss/app.scss';
 import $ from 'cash-dom';
+import Promise from 'promise-polyfill';
 
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
 export class App {
   initializeApp() {
@@ -8,7 +11,7 @@ export class App {
     $('.load-username').on('click', event => {
       let userName = $('.username.input').val();
 
-      fetch('https://api.github.com/users/' + userName)
+      fetch('https://api.github.com/users/' + this.userInput_validation(userName))
         .then(response => response.json())
         .then(body => {
           this.profile = body;
@@ -24,5 +27,17 @@ export class App {
     $('#profile-image').attr('src', this.profile.avatar_url)
     $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login)
     $('#profile-bio').text(this.profile.bio || '(no information)')
+  }
+
+  userInput_validation(userName) {
+    let reg = new RegExp('^([(a-z)|(0-9)|\-|\_])+$', 'i');
+
+    if (reg.test(userName)) {
+      $('.username.input').removeClass('unvalid');
+      return userName;
+    } else {
+      $('.username.input').addClass('unvalid');
+    }
+
   }
 }
