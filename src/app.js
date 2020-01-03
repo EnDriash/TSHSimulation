f24329dc3077a4d05eaa536cfb37437306cc7137
Jędrzej Krawczyk <siewierjedr@gmail.com>
2020-01-03 18:43:35 +0100

    Solved second task. Fetching events data and applying it dynamically

--- src/app.js
+++ src/app.js
@@ -1,25 +1,61 @@
 import './assets/scss/app.scss';
 import $ from 'cash-dom';
-import Promise from 'promise-polyfill';
+import 'es6-promise/auto';
+import 'isomorphic-fetch';
+import 'promise-polyfill';
 
-require('es6-promise').polyfill();
-require('isomorphic-fetch');
+class App {
 
-export class App {
   initializeApp() {
-
     $('.load-username').on('click', event => {
       let userName = $('.username.input').val();
+      let url = `https://api.github.com/users/${this.userInput_validation(userName)}`;
 
-      fetch('https://api.github.com/users/' + this.userInput_validation(userName))
-        .then(response => response.json())
+      fetch(url)
+        .then(response => {
+          if (!response.ok) {
+            return Promise.reject(response)
+          } else {
+            return response.json()
+          }
+        })
         .then(body => {
           this.profile = body;
           this.update_profile();
+        }).then(() => {
+
+          fetch(url +'/events/public')
+            .then(response => response.json())
+            .then(eventsList => {
+              eventsList = eventsList.filter((elem) => {
+                return elem.type.includes('PullRequest');
+              })
+              this.update_history(eventsList);
+            }).catch((error) => {
+              console.log(error);
+              window.alert('Sorry! Dosen\'t available any PullRequest event.');
+            })
+
+        }).catch((error) => {
+          console.log(error);
+          window.alert('The user doesn\'t egzist!');
         })
 
     })
 
+  }
+
+  update_history(data) {
+    console.log(data)
+    Array.from($('.timeline-item')).forEach((elem, idx) => {
+      $('.events-container .timeline-content .heading')
+        .text(data[idx].created_at.slice(0,10) + ' ' + data[idx].created_at.slice(11,16));
+      $('.events-container .gh-username img').attr('src', data[idx].actor.avatar_url);
+      $('.events-container .gh-username a').attr('href', 'https://github.com/' + data[idx].actor.login).text(data[idx].actor.login);
+      $('.events-container .content .action-type').text(data[idx].payload.pull_request.state); //OK
+      $('.events-container .content > a').attr('href', data[idx].payload.pull_request.html_url);
+      $('.events-container .content .repo-name > a').attr('href', data[idx].payload.pull_request.base.repo.html_url).text(data[idx].payload.pull_request.base.repo.full_name);
+    })
   }
 
   update_profile() {
@@ -41,3 +77,5 @@ export class App {
 
   }
 }
+
+module.exports = {App};
